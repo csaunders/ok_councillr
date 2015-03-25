3fbb78991bbe2ff568373bfd0d6a6900824caf38
CivicImages <kobewka@gmail.com>
2015-03-25 09:32:47 -0400

    WIP getting names from councillor's list page

--- lib/councillors_scraper.rb
+++ lib/councillors_scraper.rb
@@ -4,6 +4,7 @@ class CouncillorsScraper
 		@base_uri             = "http://www1.toronto.ca"
 		@councillor_list_path = "/wps/portal/contentonly?vgnextoid=c3a83293dc3ef310VgnVCM10000071d60f89RCRD"
 		@councillors          = []
+		@names = []
 	end
 
 	def get(uri)
@@ -13,12 +14,15 @@ class CouncillorsScraper
 				.to_s
 	end
 
-	def councillors_paths
+	def councillors_table
 		uri = URI(@base_uri + @councillor_list_path)
 		Loofah.document( get(uri) )
 			.scrub!(:strip)
 			.xpath('//table')
-			.css("a")
+	end
+
+	def councillors_paths
+		councillors_table.css("a")
 			.map {|a| a.attr( "href" ) if a.text.include?( "Councillor" )} #|| a.text.include?( "Mayor" ) }
 			.compact
 	end
@@ -34,19 +38,22 @@ class CouncillorsScraper
 			hash = {}
 			uri = URI(@base_uri + path)
 			doc = Loofah.document( get(uri) ).scrub!(:strip)
-			@councillors << { name:  get_name,
+			@councillors << { name:  "hi",
 				image: @base_uri + get_img_src(doc),
 				website: get_website(doc)
 			}
 		end
 	end
 
-	def get_name
-		names = []
-		councillors_paths.each do |path|
-			names << path.previous.previous.previous.text
+	def get_name(doc)
+		cells = doc.xpath('//table').css("tr").map do |tr|
+			tr.css("td").map do |td|
+				td.children.text
+			end
+		end.flatten.delete_if { |cell| cell.length < 2 }
+		cells[1..44].each do |cell|
+			@names << cell.split(" - ")[0].split(", ")
 		end
-		binding.pry
 	end
 # def get_name(doc)
 # 		doc.css("h1").text.strip.split(" ")
@@ -62,8 +69,10 @@ class CouncillorsScraper
 	end
 
 	def run
+# def get_name(doc)
 		councillor_info
-		print @councillors
+		doc = councillors_table
+		binding.pry
 	end
 end
 
