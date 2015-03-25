667da234ea5a7f36fd7c8789ae33e8ef85eb2b29
CivicImages <kobewka@gmail.com>
2015-03-24 20:34:26 -0400

    building out councillor's info scraper

--- lib/councillor_scraper.rb
+++ lib/councillor_scraper.rb
@@ -1,13 +1,29 @@
-uri                       = URI("http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=c3a83293dc3ef310VgnVCM10000071d60f89RCRD")
-request                   = Net::HTTP::Get.new(uri)
-request['Accept-Charset'] = "utf-8"
+base_uri             = "http://www1.toronto.ca"
+charset_request      = { name: 'Accept-Charset', value: "utf-8"}
+councillor_list_path = "/wps/portal/contentonly?vgnextoid=c3a83293dc3ef310VgnVCM10000071d60f89RCRD"
 
-response = Net::HTTP.start(uri.hostname, uri.port) {|http|
-	http.request(request)
-}
 
-doc = Loofah.document(response.body).scrub! :strip
+def request(uri)
+	Net::HTTP::Get.new(uri)
+		.add_field( charset_request[:name], charset_request[:value])
+end
 
-table = doc.xpath('//table')
+def response_body(uri)
+	Net::HTTP.start(uri.hostname, uri.port) {|http|
+		http.request(request(uri).body
+	}
+end
 
-urls = table.css("a").map {|a| a.attr("href") unless a.text.include? "Ward" }.compact
+def councillors_paths
+	uri = URI base_uri + councillor_list_path
+	Loofah.document(response_body(uri))
+		.scrub(:strip)
+		.xpath('//table')
+		.css("a")
+		.map {|a| a.attr("href") unless a.text.include? "Ward" }
+		.compact
+end
+
+
+
+urls.each do |path|
